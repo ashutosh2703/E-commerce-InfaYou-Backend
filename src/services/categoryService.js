294e8256e52513c1fe60dbdd69743f6b13a68307
ashutosh2703 <ashutosh.0321sharma@gmail.com>
2025-06-02 12:06:58 +0530

    new feature added

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -4,6 +4,10 @@ const categoryService = {
   // Create a new category
   createCategory: async (categoryData) => {
     try {
+      if (!categoryData.name || categoryData.name.trim() === '') {
+        throw new Error('Category name is required');
+      }
+
       const category = new CategoryModel(categoryData);
       return await category.save();
     } catch (error) {
@@ -36,6 +40,10 @@ const categoryService = {
   // Update category
   updateCategory: async (categoryId, updateData) => {
     try {
+      if (updateData.name !== undefined && updateData.name.trim() === '') {
+        throw new Error('Category name cannot be empty');
+      }
+
       const category = await CategoryModel.findByIdAndUpdate(
         categoryId,
         updateData,
