ec5b032d232f9a023b14665483a92c8033f4c723
ashutosh sharma <123867228+ashutosh2703@users.noreply.github.com>
2025-06-09 14:56:35 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const { app } = require(".");
 const { connectDb } = require("./config/db");
 
 const PORT=5454;
-app.listen(PORT,0.0.0.0,async ()=>{
+app.listen(PORT,"0.0.0.0",async ()=>{
     await connectDb()
     console.log("ecommerce api listing on port ",PORT)
 })
